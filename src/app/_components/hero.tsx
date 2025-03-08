3319ab252c1de388663f220a4766b418e1f63962
Felipe Brito <felipebrito.dev@gmail.com>
2025-03-08 19:53:03 -0300

    feat: add aboout page

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,11 +1,28 @@
 import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
 import dogImage from "../../../public/hero-dog.webp";
+import catImage from "../../../public/cat-hero.png";
 import Image from "next/image";
 
 export function Hero() {
   return (
     <section className="bg-[#E84C2D] text-white relative overflow-hidden">
-      <div className="container mx-auto py-16 px-4 relative">
+      <div>
+        <Image
+          src={dogImage}
+          alt="Foto do Cachorro"
+          fill
+          sizes="100vw"
+          priority
+          className="object-cover opacity-60 lg:hidden"
+        />
+      </div>
+
+      <div className="absolute inset-0 bg-black opacity-45 md:hidden"></div>
+
+      <div
+        className="container mx-auto pt-16 pb-16 md:pb-0
+       px-4 relative"
+      >
         <article className="grid grid-cols-1 lg:grid-cols-2 gap-8 ">
           <div className="space-y-6">
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10">
@@ -29,11 +46,30 @@ export function Hero() {
                 <b className="bg-black text-white px-2 py-1 rounded-md">5%</b>{" "}
                 de desconto na primeira compra.
               </p>
+
+              <div className="flex mt-4">
+                <div className="w-32 hidden lg:block">
+                  <Image
+                    src={catImage}
+                    alt="Foto do gato"
+                    quality={100}
+                    className="object-fill"
+                  />
+                </div>
+              </div>
             </div>
           </div>
 
-          <div className="hidden md:block h-full">
-            <Image src={dogImage} alt="Foto do dog" />
+          <div className="hidden md:block h-full relative">
+            <Image
+              src={dogImage}
+              alt="Foto do dog"
+              className="object-contain"
+              fill
+              sizes="(max-width: 768px) 0vw, 50vw"
+              quality={100}
+              priority
+            />
           </div>
         </article>
       </div>
